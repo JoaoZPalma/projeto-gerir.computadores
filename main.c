e1c298d9570a118ea35c141ab3a2b7d94a11e941
SuperWarrior2608 <99098141+SuperWarrior2608@users.noreply.github.com>
2022-12-22 16:25:45 +0000

    Update main.c
    Criacao de submenu de registo e algumas alteracoes

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "funcoes_portatil.h"
+#include "constantes1.h"
 #include "funcoes_auxiliares1.h"
+#include "funcoes_portatil.h"
+#include "funcoes_requisicao.h"
 
 int menuPrincipal();
 
@@ -10,51 +12,74 @@ int main()
 {
     int quantidadePortateisRegistados = 0;
     int quantidadePortateisRequisitados = 0;
-    int opcaoPrincipal,portateisDisponiveis,portateisRequisitados;
+    int quantidadePortateisAvariados = 0;
+    int opcaoPrincipal, portateisDisponiveis, portateisRequisitados, opcaoRegisto;
     tipoPortatil vetorPortateis[MAX_PORTATEIS];
 
     do
     {
-    portateisDisponiveis = quantidadePortateisRegistados-quantidadePortateisRequisitados;
+        portateisDisponiveis = quantidadePortateisRegistados-quantidadePortateisRequisitados-quantidadePortateisAvariados;
 
 
-    opcaoPrincipal = menuPrincipal();
-    switch (opcaoPrincipal)
-    {
-    case 1: //Realizar requisição de 1 computador
-        registarPortatil(vetorPortateis,quantidadePortateisRegistados);
-        mostrarInformacaoPortatilPorPosicao(vetorPortateis,quantidadePortateisRegistados);
-        quantidadePortateisRegistados++;
-        break;
-    case 2:
-        break;
-    case 3:
-        break;
-    case 4:
-        break;
-    case 5:
-        break;
-    case 6:
-        break;
-    case 7:
-        break;
-    case 8:
-        break;
-    case 9:
-        break;
-    case 10:
-        break;
-    case 11:
-        break;
-    case 12:
-        break;
-    case 0:
-        break;
-    default:
-         printf ("Opcao Invalida\n");
+        opcaoPrincipal = menuPrincipal();
+        switch (opcaoPrincipal)
+        {
+        case 1: //Realizar requisiÃ§Ã£o de 1 computador
+            do
+            {
+                opcaoRegisto = menuRegistoPortatil();
+                switch (opcaoRegisto)
+                {
+                case 1:
+                    registarPortatil(vetorPortateis,quantidadePortateisRegistados);
+                    quantidadePortateisRegistados++;
+                    break;
+                case 2:
+                    mostrarInformacaoPortatilPorPosicao(vetorPortateis,quantidadePortateisRegistados);
+                    break;
+                case 0:
+                    printf("\nA voltar ao menu anterior\n\n");
+                    break;
+                default:
+                    printf ("Opcao Invalida\n");
+                    break;
+                }
+            }
+            while(opcaoRegisto != 0);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
+        case 0:
+            printf("A terminar programa!\n");
+            break;
+        default:
+            printf ("Opcao Invalida\n");
+            break;
+
+        }
 
     }
-    }while(opcaoPrincipal != '0');
+    while(opcaoPrincipal != 0);
+
+    return 0;
 }
 
 int menuPrincipal()
